f0790f407676e3f75cf071336aa6101659d4a457
Andrew Petersen <kirbysayshi@gmail.com>
2026-04-26 02:09:10 -0400

    incredibly, ruffle-rs has brought my swfs back from the dead

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,37 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [
+    {
+      // This is specifically to simulate ghpages' ability to default load
+      // index.html, which happens for the old old flash files :)
+      name: 'serve-public-index',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          const url = req.url?.split('?')[0];
+          if (!url) {
+            next();
+            return;
+          }
+          const candidate = path.join(
+            process.cwd(),
+            'public',
+            url,
+            'index.html',
+          );
+          if (fs.existsSync(candidate)) {
+            res.setHeader('Content-Type', 'text/html');
+            fs.createReadStream(candidate).pipe(res);
+            return;
+          }
+          next();
+        });
+      },
+    },
+    reactRouter(),
+  ],
 });
