82b235730129d1415500c526551786876cfeec62
Andrew Petersen <kirbysayshi@gmail.com>
2026-05-08 22:56:33 -0400

    move cdn video to rehype plugin, keep react integration for later

--- app/lib/posts.ts
+++ app/lib/posts.ts
@@ -1,5 +1,8 @@
+import path from 'node:path';
+
 import rehypeShiki from '@shikijs/rehype';
 import type { Element, Root as HastRoot } from 'hast';
+import { h } from 'hastscript';
 import type { Root as MdastRoot } from 'mdast';
 import rehypeExternalLinks from 'rehype-external-links';
 import rehypeRaw from 'rehype-raw';
@@ -9,10 +12,10 @@ import remarkGfm from 'remark-gfm';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { type Plugin, unified } from 'unified';
-import { visit } from 'unist-util-visit';
+import { SKIP, visit } from 'unist-util-visit';
 import { parse } from 'yaml';
 
-import { CDNVideoPlayer } from '../components/CDNVideoPlayer.js';
+import { cdnUrl, MediaManifest } from './media.js';
 import {
   type PostFrontmatter,
   postFrontmatterSchema,
@@ -98,6 +101,7 @@ const rehypePostLinks = () => (tree: HastRoot) => {
       typeof href !== 'string' ||
       href[0] === '/' ||
       /^[a-z][a-z+\-.]*:/i.test(href) ||
+      !/\.(markdown|md)^/i.test(href) ||
       href[0] === '#'
     )
       return;
@@ -105,6 +109,83 @@ const rehypePostLinks = () => (tree: HastRoot) => {
     const parsed = parsePostFilename(href);
     if (parsed.error) throw parsed.error;
     node.properties.href = `/${parsed.year}/${parsed.month}/${parsed.day}/${parsed.slug}.html`;
+  });
+};
+
+/**
+ * Coerce to React/hast-compatible values (e.g. `attr="false"` -> `attr: false`
+ * to _remove_ that attribute from the HTML). Authoring JSX or hyperscript
+ * wouldn't need this normally: we're accepting markdown-authored HTML, where
+ * everything is a string.
+ */
+function coerceHTMLLikeProperties(props: Record<string, unknown>) {
+  for (const [key, val] of Object.entries(props)) {
+    if (typeof val !== 'string') continue;
+    if (val === 'true') props[key] = true;
+    else if (val === 'false') props[key] = false;
+    else if (val.match(/0-9+\.0-9*/)) props[key] = parseFloat(val);
+    else if (val.match(/0-9/)) props[key] = parseInt(val, 10);
+  }
+
+  return props;
+}
+
+const rehypeCDNVideo = () => (tree: HastRoot) => {
+  visit(tree, 'element', (node: Element, index, parent) => {
+    if (
+      !parent ||
+      index === undefined ||
+      node.tagName !== 'video' ||
+      !node.properties.src?.toString().match(/\.json$/)
+    ) {
+      return;
+    }
+
+    const manifests = import.meta.glob<MediaManifest>('../../videos/*.json', {
+      import: 'default',
+      eager: true,
+    });
+
+    if (typeof node.properties.src !== 'string')
+      throw new Error('src must be a string!');
+
+    const slug = path.basename(node.properties.src, '.json');
+    const data = Object.values(manifests).find((m) => m.slug === slug);
+    if (!data) throw new Error(`Could not find manifest for slug ${slug}`);
+    const manifest = MediaManifest.parse(data);
+
+    const plus = manifest.variants.find((v) => v.label.includes('+'));
+    const largest =
+      plus ?? manifest.variants.sort((a, b) => b.height - a.height).at(0);
+
+    if (!largest) throw new Error(`No largest variant for ${slug}`);
+
+    const nextProps = coerceHTMLLikeProperties({ ...node.properties });
+    delete nextProps.src;
+
+    const video = h(
+      'video',
+      {
+        controls: true,
+        playsInline: true,
+        preload: 'metadata',
+        style: {
+          'aspect-ratio': `${String(largest.width)} / ${String(largest.height)}`,
+          width: '100%',
+        },
+        ...nextProps,
+      },
+      h('source', {
+        src: cdnUrl(largest.pathName),
+        type: largest.contentType,
+      }),
+    );
+
+    const figure = h('figure', {}, video, h('figcaption', node.children));
+
+    const next = node.children.length > 0 ? figure : video;
+    parent.children.splice(index, 1, next);
+    return [SKIP, index + 1];
   });
 };
 
@@ -129,9 +210,10 @@ function buildProcessor() {
     .use(rehypeRaw)
     .use(rehypeReactComponents, {
       components: {
-        'cdn-video-player': CDNVideoPlayer,
+        // tag-name : TagName
       },
     })
+    .use(rehypeCDNVideo)
     .use(rehypePostLinks)
     .use(rehypeExternalLinks)
     .use(rehypeShiki, {
